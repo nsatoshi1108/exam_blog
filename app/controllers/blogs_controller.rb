f823980c98c434a0d4ee7aa718d9a2a8f3c28c18
satoshi <consultation.pharmcist2@gmail.com>
2018-07-25 22:35:11 +0900

    bootstrap課題提出

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,14 @@
 class BlogsController < ApplicationController
-  
+
   before_action :blog_id, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @blogs = Blog.all
   end
-  
+
+  def top
+  end
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -14,23 +17,23 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
 
   def new
     @blog = Blog.new
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path ,notice: 'ブログを編集しました'
@@ -38,19 +41,19 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:name,:content)
   end
-  
+
   def blog_id
     @blog = Blog.find(params[:id])
   end
-  
+
 end
